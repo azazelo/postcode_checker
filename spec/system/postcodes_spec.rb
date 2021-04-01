3b8005e3b1c8739b20c7e2b1c2e433f21357e826
Andrey Eremeev <andrey.eremeyev@gmail.com>
2021-04-02 00:59:50 +0400

    add filed saving postcode spec

--- spec/system/postcodes_spec.rb
+++ spec/system/postcodes_spec.rb
@@ -6,7 +6,7 @@ describe 'User interface', :aggregate_failures do
   feature 'Save postcode to allowed postcodes list', %(
     To save postcode as a User I want to be able to enter
     postcode in text_field and click on button [#{add_postcode_button_text}]) do
-    let(:postcode_value) { 'SE17QD' }
+    let(:postcode_value) { 'SH241AA' }
 
     scenario 'User type postcode in text_field and click on Submit button', :web_ui do
       visit new_postcode_path
@@ -14,6 +14,17 @@ describe 'User interface', :aggregate_failures do
       page.fill_in 'postcode[value]', with: postcode_value
       click_button add_postcode_button_text
       expect(page).to have_content "Postcode '#{postcode_value}' Saved"
+    end
+
+    before do 
+      Postcode.create(value: 'SH241AB')
+    end
+    scenario 'User type existing postcode in text_field, click on Submit button', :web_ui do
+      visit new_postcode_path
+      expect(page).to have_content 'Add Postcode'
+      page.fill_in 'postcode[value]', with: 'SH241AB'
+      click_button add_postcode_button_text
+      expect(page).to have_content "Value has already been taken"
     end
 
     scenario 'User wants to see Allowed Postcodes', :web_ui do
